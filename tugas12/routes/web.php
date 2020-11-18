08d5918b8ed40c9cfc08a602067060fa37e41ba8
Dandimanz <dandimansyah@gmail.com>
2020-11-18 22:49:05 +0700

    Tugas 13

--- tugas12/routes/web.php
+++ tugas12/routes/web.php
@@ -3,8 +3,20 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 
 Route::get('/register', 'AuthController@daftar');
 
 Route::post('/welcome', 'AuthController@datang');
+
+Route::get('/master', function () {
+    return view('adminLTE.master');
+});
+
+Route::get('/', function () {
+    return view('adminLTE.items.index');
+});
+
+Route::get('/data-tables', function () {
+    return view('adminLTE.items.data-tables');
+});
